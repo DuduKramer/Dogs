e537304e811867c8d95e61fd19936cc0f50aef03
Eduardo <dududeoliveirakramer23@gmail.com>
2025-05-26 15:46:53 -0300

    Assets and routes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import Dogs from '../Assets/dogs.svg?react';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      Header
+      <nav className="container">
+        <Link className={styles.logo} to="/" aria-label="Dog">
+          {' '}
+          <Dogs />{' '}
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
