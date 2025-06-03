a768d33bc101c5245d941016edfcaf4ea6aaa309
Eduardo <dududeoliveirakramer23@gmail.com>
2025-06-03 16:27:56 -0300

    queijo

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,14 +3,30 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST } from '../../api';
+import { TOKEN_POST, USER_GET } from '../../api';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      getUser(token);
+    }
+  }
+  , []);
+
+  async function getUser(token){
+    const {url, options} = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
   async function handleSubmit(event) {
     event.preventDefault();
+    console.log('handleSubmit');
 
     if (username.validate() && password.validate()) {
       const { url, options } = TOKEN_POST({
@@ -22,6 +38,7 @@ const LoginForm = () => {
       const response = await fetch(url, options);
       const json = await response.json();
       window.localStorage.setItem('token', json.token);
+      getUser(json.token);
     }
   }
 
