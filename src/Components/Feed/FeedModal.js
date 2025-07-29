93a9518dd190994195825e06c644ccae01e51cb2
Eduardo Kramer <dududeoliveirakramer23@gmail.com>
2025-07-29 00:40:56 -0300

    feedmodal

---
+++ src/Components/Feed/FeedModal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../Hooks/useFetch';
+
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+  
+  return <div className={styles.modal}>FeedModal</div>;
+};
+
+export default FeedModal;
