bead28ff4e38f57e720a26ea5edaa47f326f7c44
Eduardo <dududeoliveirakramer23@gmail.com>
2025-08-19 14:55:12 -0300

    end modal

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -14,7 +14,7 @@ const PhotoContent = ({ data }) => {
         <div>
           <p className={styles.author}>
             <Link  to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-            <span className={styles.visualizações}>{photo.acessps}</span>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className='title'>
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
