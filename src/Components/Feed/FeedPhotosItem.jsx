b21b0bbf135098845daa8c9701b34ab476da9042
Eduardo <dududeoliveirakramer23@gmail.com>
2025-08-25 17:30:44 -0300

    Photo Delete and Loading Skeleton

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 import styles from './FeedPhotosItem.module.css';
+import Image from '../Helper/Image';
 
-const FeedPhotosItem = ({photo, setModalPhoto}) => {
-  function handleClick(){
-    setModalPhoto(photo)
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.vizualizacao}>{photo.acessos}</span>
     </li>
-  )
-}
+  );
+};
 
-export default FeedPhotosItem
+export default FeedPhotosItem;
