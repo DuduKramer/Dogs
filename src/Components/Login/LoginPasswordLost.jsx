80267559a456f243b7145cb55feb116e094f0fee
Eduardo <dududeoliveirakramer23@gmail.com>
2025-09-15 15:14:10 -0300

    perdeu a senha complete

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,29 +1,42 @@
-import React from "react";
-import Input from "../Forms/Input";
-import Button from "../Forms/Button";
-import useForm from "../../Hooks/useForm";
-import useFetch from "../../Hooks/useFetch";
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
-
-  const email = useForm();
-  const [data, loading, error, request] = useFetch();
-
-  
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
-
-    request(URL, option)
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace('perdeu', 'resetar'),
+    });
+    const { json } = await request(url, options);
+    console.log(json);
   }
 
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
-      <form onSubmit={handleSubmit}>
-        <Input label="Email / Usuario" type="text" name="email" />
-        <Button>Enviar Email</Button>
-      </form>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuario" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
     </section>
   );
 };
